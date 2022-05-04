880d567d0aa765e7431c37904b2d7a3f39c05d52
Ayrton Sato <ayrton_sato@hotmail.com>
2022-05-04 17:00:34 -0300

    done

--- lib/exmeal/user/create.ex
+++ lib/exmeal/user/create.ex
@@ -1,6 +1,16 @@
 defmodule Exmeal.Users.Create do
+  alias Exmeal.{Error, Repo, User}
 
-  def call() do
-    # TO DO
+  def call(params) do
+    params
+    |> User.changeset()
+    |> Repo.insert()
+    |> handle_insert()
   end
+
+  defp handle_insert({:error, result}) do
+    {:error, Error.build(:bad_request, result)}
+  end
+
+  defp handle_insert(result), do: result
 end
--- lib/exmeal/user/update.ex
+++ lib/exmeal/user/update.ex
@@ -1,7 +1,16 @@
 defmodule Exmeal.Users.Update do
-  alias Exmeal.{Error, Repo, User}
+  alias Exmeal.{Repo, Error, User}
 
   def call(%{"id" => id} = params) do
-    # TO DO
+    case Repo.get(User, id) do
+      nil -> {:error, Error.build_user_not_found_error()}
+      user -> do_update(user, params)
+    end
+  end
+
+  defp do_update(user, params) do
+    user
+    |> User.changeset(params)
+    |> Repo.update()
   end
 end
